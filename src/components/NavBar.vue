484ea8e7c787e8430deff61bac3f2aefc6f1f0be
Makar_Jv <makar_igor1997@mail.ru>
2023-03-26 22:16:14 +0300

    init commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+	<nav>
+		<div class="openMenu">
+			<ion-icon name="menu" class="openBtn"></ion-icon>
+			<ion-icon name="close" class="closeBtn"></ion-icon>
+		</div>
+	</nav>
+	<div class="group">
+		<ul>
+			<li><a href="#" class="homePage">Home</a></li>
+			<li><a href="#" class="aboutMe">About Me</a></li>
+			<li><a href="#">Setting</a></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	mounted() {
+		let openMenu = document.querySelector('.openMenu')
+		let closeBtn = document.querySelector('.closeBtn')
+		let openBtn = document.querySelector('.openBtn')
+		let group = document.querySelector('.group')
+		
+		openBtn.onclick = function () {
+			openMenu.classList.add('open')
+			group.classList.add('open')
+		}
+		closeBtn.onclick = function () {
+			openMenu.classList.remove('open')
+			group.classList.remove('open')
+		}
+	}
+}
+</script>
